5584e95c143c88977032a4ff8a89ba8806dce30a
Gerry Skerbitz <gsker@users.noreply.github.com>
2020-11-18 14:44:51 -0600

    Update icu_replace.sh
    pkg-config instead of icu-config

--- icu_replace.sh
+++ icu_replace.sh
@@ -1,2 +1,2 @@
-gcc -fPIC -O2 -Wall -shared -o icu_replace.so icu_replace.c $(pkg-config --libs icu-i18n`icu-config)
+gcc -fPIC -O2 -Wall -shared -o icu_replace.so icu_replace.c $(pkg-config --libs icu-i18n)
 strip icu_replace.so
